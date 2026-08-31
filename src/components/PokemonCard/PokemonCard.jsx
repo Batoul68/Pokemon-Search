db92bbeea755bacea5d8b26bf64e5b384b2a2123
Batoul <batoulalso68@gmail.com>
2026-08-31 12:47:57 -0400

    simplified PokemonCard conditional rendering

--- src/components/PokemonCard/PokemonCard.jsx
+++ src/components/PokemonCard/PokemonCard.jsx
@@ -1,40 +1,39 @@
-import React, { useState, useEffect } from 'react';
 import defaultPokemonImg from '../../assets/placeholder-pokemon.png';
 import unknownPokemonImg from '../../assets/unknown-pokemon.png';
 import styles from './PokemonCard.module.css';
 
 export default function PokemonCard({pokemon}) {
 
-  const [pokemonName, setPokemonName] = useState();
-  const [pokemonType, setPokemonType] = useState();
-  const [pokemonSprite, setPokemonSprite] = useState();
+  let displayPokemon;
   
-  useEffect(() => {
-
-    // Default pokemon card
-    if (pokemon === undefined || pokemon.previous === null) {
-      setPokemonName('pokémon');
-      setPokemonType('pokemon type');
-      setPokemonSprite(defaultPokemonImg);
+  // Default pokemon card
+  if (pokemon === undefined || pokemon.previous === null) {
+    displayPokemon = {
+      name: 'pokémon',
+      type: 'pokemon type',
+      sprite: defaultPokemonImg
     }
-    else if (pokemon === 'unknown') {
-      setPokemonName('unknown');
-      setPokemonType('unknown type');
-      setPokemonSprite(unknownPokemonImg);
+  }
+  else if (pokemon === 'unknown') {
+    displayPokemon = {
+      name: 'unknown',
+      type: 'unknown type',
+      sprite: unknownPokemonImg
     }
-    else {
-      setPokemonName(pokemon?.name);
-      setPokemonType(pokemon?.types?.[0]?.type?.name);
-      setPokemonSprite(pokemon?.sprites?.front_default);
+  }
+  else {
+    displayPokemon = {
+      name: pokemon?.name,
+      type: pokemon?.types?.[0]?.type?.name,
+      sprite: pokemon?.sprites?.front_default
     }
-  }, [pokemon]);
-  
+  }
 
   return(
     <div className={styles.card}>
-      <h2 id={styles['card-title']}>{pokemonName}</h2>
-      <img id={styles['card-image']} alt="Pokémon Image" src={pokemonSprite} />
-      <p id={styles['card-text']}>{pokemonType}</p>
+      <h2 id={styles['card-title']}>{displayPokemon.name}</h2>
+      <img id={styles['card-image']} alt="Pokémon Image" src={displayPokemon.sprite} />
+      <p id={styles['card-text']}>{displayPokemon.type}</p>
     </div>
   );
 }
